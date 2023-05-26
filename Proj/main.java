b5afe72ab675456a4b948c426deb328608dfb812
Mohameedshawky <133530678+Mohameedshawky@users.noreply.github.com>
2023-05-26 16:11:25 +0300

    Update main.java

--- Proj/main.java
+++ Proj/main.java
@@ -140,3 +140,123 @@ public class main extends Application
             c.setFill(Color.BLACK);
             p.getChildren().add(c);
         }
+        int f5=0;
+        for(int z5=0;z5<x3.size();++z5)
+        {
+            Double te=0.0;
+            Double ds=-1.0;
+            for (int z6=0;z6<x4.size();++z6)
+            {
+                if(x4.get(z6).equals(ds))
+                {
+                    z6=z6+f5;
+                    ++f5;
+                    continue;
+                }
+                if(x4.get(z6).equals(te))
+                {
+                    break;
+                }
+                if(y3.get(z5).equals(y4.get(z6)))
+                {
+                    Line l8=new Line(x3.get(z5),y3.get(z5),x4.get(z6),y4.get(z6));
+                    l8.setStrokeWidth(1.3);
+                    p.getChildren().add(l8);
+                    ++f5;
+                    Polygon triangle = new Polygon();
+                triangle.getPoints().addAll(
+                x4.get(z6), y4.get(z6), 
+                x4.get(z6)-8, y4.get(z6)-6, 
+                x4.get(z6)-8,  y4.get(z6)+6
+        );
+               triangle.setFill(Color.BLACK);
+               p.getChildren().add(triangle);
+                    x4.set(z6,ds);
+                    y4.set(z6,ds);
+                }
+                else
+                {
+                    Line l6=new Line(x3.get(z5),y3.get(z5),x3.get(z5),y4.get(z6));
+                    l6.setStrokeWidth(1.3);
+                    p.getChildren().add(l6);
+                    Line l7=new Line(x3.get(z5),y4.get(z6),x4.get(z6),y4.get(z6));
+                    l7.setStrokeWidth(1.3);
+                    p.getChildren().add(l7);
+                    ++f5;
+                    Polygon triangle = new Polygon();
+                triangle.getPoints().addAll(
+                x4.get(z6), y4.get(z6), 
+                x6.get(z6), y4.get(z6)-6, 
+                x6.get(z6),  y4.get(z6)+6
+        );
+               triangle.setFill(Color.BLACK);
+               p.getChildren().add(triangle);
+                    x4.set(z6,ds);
+                    y4.set(z6,ds);
+                }
+            }
+        }
+        Scene s = new Scene(p, 1300, 600);
+        primaryStage.setTitle("SiMULINK");
+        primaryStage.setScene(s);
+        primaryStage.show();
+    }
+    public static void main(String[] args)
+    {
+        try{
+        String fi=args[0];
+        File z=new File(fi);
+        if (!fi.endsWith(".mdl")) 
+            {
+                throw new NotVaildAutosarFileException("Invalid file extension!");
+            }
+            if(z.length()==0)
+            {
+                throw new EmptyAutosarFileException("Empty file!");
+            }
+        FileInputStream file=new FileInputStream(fi);
+        int data;
+        StringBuilder x=new StringBuilder();
+        while((data = file.read())!=-1)
+            {
+                x.append((char)data);
+            }
+        String d=x.toString();
+        Scanner p = new Scanner(d);
+        FileOutputStream e=new FileOutputStream("Example.xml");
+        while(p.hasNextLine())
+            {
+                String a=p.nextLine();
+                if((a.contains("<System>")))
+                {
+                    while(!(a.contains("</System>")))
+                    {
+                        e.write((a+"\n").getBytes());
+                        a=p.nextLine();
+                    }
+                }
+            }
+            e.write("</System>".getBytes());
+        FileInputStream last=new FileInputStream("Example.xml");
+        int u;
+        StringBuilder y=new StringBuilder();
+        while((u = last.read())!=-1)
+            {
+                y.append((char)u);
+            }
+        String o=y.toString();
+        Scanner j = new Scanner(o);
+        Scanner u1 = new Scanner(o);
+        ArrayList<Double> lef=new ArrayList<>();
+        ArrayList<Double> to=new ArrayList<>();
+        ArrayList<String> nam=new ArrayList<>();
+        ArrayList<String> mir=new ArrayList<>();
+        ArrayList<String> id=new ArrayList<>();
+        ArrayList<Integer> pn=new ArrayList<>();
+        ArrayList<String> src=new ArrayList<>();
+        ArrayList<String> dst=new ArrayList<>();
+        ArrayList<String> dst_b=new ArrayList<>();
+        ArrayList<String> in=new ArrayList<>();
+        ArrayList<String> in_b=new ArrayList<>();
+        ArrayList<String> addi=new ArrayList<>();
+        ArrayList<Integer> addi_1=new ArrayList<>();
